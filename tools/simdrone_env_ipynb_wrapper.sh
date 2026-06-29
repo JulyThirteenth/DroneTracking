0cbc185bc85dbd83efda3b01d5431b37441ff012
PipeYume <1178795412@qq.com>
2026-06-29 16:44:23 +0800

    Delete simdrone_env_ipynb_wrapper.sh

--- tools/simdrone_env_ipynb_wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source /opt/conda/etc/profile.d/conda.sh
-eval "$(conda shell.bash hook)"
-conda activate mpcc
-source /opt/ros/humble/setup.bash
-source ~/IsaacSim-ros_workspaces/humble_ws/install/setup.bash
-exec "$CONDA_PREFIX/bin/python" -m ipykernel_launcher "$@"
